341283cd8b8f9c801609933c0572fc6455e787ab
Aditya Sharma <102215488+aditya2410-U@users.noreply.github.com>
2025-01-20 14:14:45 +0530

    Time: 0 ms (100%), Space: 13.4 MB (39.32%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,20 +5,26 @@ public:
         int m = matrix[0].size();
         int low = 0;
         int high = n * m  -1;
+
         while( low <= high){
             int mid = (low + high) / 2;
             int row = mid/m;
             int col = mid%m;
+
             if(matrix[row][col] < target){
+
                 low = mid + 1;
             }
             else if(matrix[row][col] > target){
+
                 high = mid - 1;
             }
             else if(matrix[row][col] == target){
+
                 return true;
             }
         }
+
         return false;
     }
 };
