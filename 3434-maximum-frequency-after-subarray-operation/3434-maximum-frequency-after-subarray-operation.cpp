89dc36704128b9fd889b1d24952f28bde2e5defd
Aditya Sharma <102215488+aditya2410-U@users.noreply.github.com>
2026-03-21 00:02:13 +0530

    Time: 46 ms (74.4%), Space: 97.1 MB (25.6%) - LeetHub

---
+++ 3434-maximum-frequency-after-subarray-operation/3434-maximum-frequency-after-subarray-operation.cpp
@@ -0,0 +1,50 @@
+
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        unordered_map<int, int> totalFreq;
+
+        // Count frequency of each number
+        for (int i = 0; i < nums.size(); i++) {
+            totalFreq[nums[i]]++;
+        }
+
+        int ans = totalFreq[k]; // base count of k
+
+        // Try converting each value 'v' into k
+        for (auto it = totalFreq.begin(); it != totalFreq.end(); it++) {
+            int v = it->first;
+
+            if (v == k) continue;
+
+            int curr = 0;
+            int best = 0;
+
+            for (int i = 0; i < nums.size(); i++) {
+                if (nums[i] == v) {
+                    curr += 1;     // we can convert v → k
+                } else if (nums[i] == k) {
+                    curr -= 1;     // we lose an existing k
+                } else {
+                    // no effect
+                }
+
+                if (curr < 0) {
+                    curr = 0;
+                }
+
+                if (curr > best) {
+                    best = curr;
+                }
+            }
+
+            // base k + best gain from converting v
+            if (totalFreq[k] + best > ans) {
+                ans = totalFreq[k] + best;
+            }
+        }
+
+        return ans;
+    }
+};
